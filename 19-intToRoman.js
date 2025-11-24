125193ac3a183a96528e5f644832474a5945d7e5
Jhonny Ramos <onlyjhonny01@gmail.com>
2025-11-24 08:21:40 -0400

    add: Implement intToRoman function to convert an integer to a Roman numeral

---
+++ 19-intToRoman.js
@@ -0,0 +1,14 @@
+var intToRoman = function(num) {
+    const val = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+    const syms = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+    let romanNumeral = ''
+
+    // Iterate through each symbol, stopping if num becomes 0.
+    for (let i = 0; i < val.length; i++) {
+        while (num >= val[i]) {
+            num -= val[i]
+            romanNumeral += syms[i]
+        }
+    }
+    return romanNumeral
+}
