7ea2fdaf4d1e624a4a877d3f33e5e6f1a4e12b46
Jhonny Ramos <onlyjhonny01@gmail.com>
2025-11-26 21:32:52 -0400

    add: Implement threeSum function to find all unique triplets in an array that sum to zero

---
+++ 21-ThreSums.js
@@ -0,0 +1,32 @@
+var threeSum = function(nums) {
+    const result = []
+    nums.sort((a, b) => a - b)
+
+    for (let i = 0; i < nums.length - 2; i++) {
+        if (i > 0 && nums[i] === nums[i - 1]) continue // Skip duplicate values for i
+
+        let left = i + 1
+        let right = nums.length - 1
+
+        while (left < right) {
+            const sum = nums[i] + nums[left] + nums[right];
+
+            if (sum === 0) {
+                result.push([nums[i], nums[left], nums[right]])
+
+                // Skip duplicate values for left and right
+                while (left < right && nums[left] === nums[left + 1]) left++
+                while (left < right && nums[right] === nums[right - 1]) right--
+
+                left++
+                right--
+            } else if (sum < 0) {
+                left++
+            } else {
+                right--
+            }
+        }
+    }
+
+    return result
+}
